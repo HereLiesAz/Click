18fefa24fb30098ed1d9a03666095efeb8611327
Az <103241502+HereLiesAz@users.noreply.github.com>
2025-09-27 00:04:23 -0500

    Update MainActivity.kt

--- app/src/main/kotlin/com/hereliesaz/click/MainActivity.kt
+++ app/src/main/kotlin/com/hereliesaz/click/MainActivity.kt
@@ -1,63 +1,62 @@
-package com.hereliesaz.click;
+package com.hereliesaz.click
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
+    private lateinit var serviceStatusText: TextView
 
-    private TextView serviceStatusText;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        serviceStatusText = findViewById(R.id.service_status_text)
+        val enableServiceButton: Button = findViewById(R.id.enable_service_button)
 
-        serviceStatusText = findViewById(R.id.service_status_text);
-        Button enableServiceButton = findViewById(R.id.enable_service_button);
-
-        enableServiceButton.setOnClickListener(v -> {
+        enableServiceButton.setOnClickListener {
             // Open the accessibility settings screen
-            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-            startActivity(intent);
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        updateServiceStatus();
-    }
-
-    private void updateServiceStatus() {
-        if (isAccessibilityServiceEnabled(this, ClickAccessibilityService.class)) {
-            serviceStatusText.setText("Service Status: Enabled");
-            serviceStatusText.setBackgroundColor(0xFFC8E6C9); // A pleasant green
-        } else {
-            serviceStatusText.setText("Service Status: Disabled");
-            serviceStatusText.setBackgroundColor(0xFFFFCDD2); // A cautionary red
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            startActivity(intent)
         }
     }
 
-    public static boolean isAccessibilityServiceEnabled(Context context, Class<?> accessibilityService) {
-        TextUtils.SimpleStringSplitter colonSplitter = new TextUtils.SimpleStringSplitter(':');
-        String settingValue = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
-        if (settingValue != null) {
-            colonSplitter.setString(settingValue);
-            while (colonSplitter.hasNext()) {
-                String componentName = colonSplitter.next();
-                if (componentName.equalsIgnoreCase(context.getPackageName() + "/" + accessibilityService.getName())) {
-                    return true;
+    override fun onResume() {
+        super.onResume()
+        updateServiceStatus()
+    }
+
+    private fun updateServiceStatus() {
+        if (isAccessibilityServiceEnabled(this, ClickAccessibilityService::class.java)) {
+            serviceStatusText.text = "Service Status: Enabled"
+            serviceStatusText.setBackgroundColor(0xFFC8E6C9.toInt()) // A pleasant green
+        } else {
+            serviceStatusText.text = "Service Status: Disabled"
+            serviceStatusText.setBackgroundColor(0xFFFFCDD2.toInt()) // A cautionary red
+        }
+    }
+
+    companion object {
+        fun isAccessibilityServiceEnabled(context: Context, accessibilityService: Class<*>): Boolean {
+            val colonSplitter = TextUtils.SimpleStringSplitter(':')
+            val settingValue = Settings.Secure.getString(context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+
+            settingValue?.let {
+                colonSplitter.setString(it)
+                while (colonSplitter.hasNext()) {
+                    val componentName = colonSplitter.next()
+                    if (componentName.equals(context.packageName + "/" + accessibilityService.name, ignoreCase = true)) {
+                        return true
+                    }
                 }
             }
+            return false
         }
-        return false;
     }
 }
