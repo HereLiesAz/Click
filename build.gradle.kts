ac09a150af40ebc3b59783b7c4ee94c99904923e
Az <103241502+HereLiesAz@users.noreply.github.com>
2025-09-27 05:00:36 -0500

    Update and rename build.gradle to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven(https://jitpack.com)
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.2.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
+        classpath 'com.android.tools.build:gradle:8.13.0'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20"
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
